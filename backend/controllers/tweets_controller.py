75245b43fa0cbd71b1e54ceb463a8f532c9530ed
ranshamay89 <ran.shamay89@gmail.com>
2017-10-26 02:39:19 +0300

    init api

---
+++ backend/controllers/tweets_controller.py
@@ -0,0 +1,24 @@
+from controllers.Controller import Controller
+from deep_bb import constants
+from deep_bb.generate_tweet import MccTweetGenerator
+from utils import tweeter_utils
+import os
+
+DEEP_BB_PREFIX_PATH = '../deep_bb/'
+
+
+class TweetController(Controller):
+    def __init__(self):
+        Controller.__init__(self)
+        self._api = tweeter_utils.TweeterUtils()
+        os.chdir(DEEP_BB_PREFIX_PATH)
+        with open(constants.CORPUS_PATH, 'r') as corpus:
+            text = corpus.read().lower()
+        self.mcc_speech_generator = MccTweetGenerator().preprocess(text)
+
+    def _generate_random_tweet(self):
+        return self.mcc_speech_generator.generate_tweet()
+
+    def _post_tweet(self):
+        msg = self._api.tweet(self._generate_random_tweet())
+        return msg
---
+++ backend/utils/tweeter_utils.py
@@ -0,0 +1,21 @@
+import tweepy
+
+CUSTOMER_API_KEY = 'o5zzr3iv6lpDmzzsQZrgjkjrW'
+CUSTOMER_SECRET = 'PrKZdQ3rp3zR1u4uXl9tfIt4yV1MJ4rIN89tdmV60MsiHgpKiu'
+
+ACCESS_TOKEN = '920207242376499200-phc1Ojhy9yRVtbdiWPxMfK2SUf0WJQz'
+ACCESS_TOKEN_SECRET = 'ePEHRNCizCpFYxYHbc7scHo2HwbA12w1Po41JTEHaHZhj'
+
+
+class TweeterUtils:
+    def __init__(self):
+        self._api = self.get_api()
+
+    def get_api(self):
+        auth = tweepy.OAuthHandler(CUSTOMER_API_KEY, CUSTOMER_SECRET)
+        auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+        return tweepy.API(auth)
+
+    def tweet(self, msg):
+        status = self._api.update_status(msg)
+        return status
