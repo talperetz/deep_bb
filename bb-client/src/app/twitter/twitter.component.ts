3f7543e8ba5f573e3242b723cb868249d9b5a9bc
Dorpz <sdorperetz@gmail.com>
2017-10-26 12:07:32 +0300

    fixed twitter update bug

--- bb-client/src/app/twitter/twitter.component.ts
+++ bb-client/src/app/twitter/twitter.component.ts
@@ -1,28 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Http, Response} from "@angular/http";
+import {Router} from "@angular/router";
 
 @Component({
-  selector: 'app-twitter',
-  templateUrl: './twitter.component.html',
-  styleUrls: ['./twitter.component.css']
+    selector: 'app-twitter',
+    templateUrl: './twitter.component.html',
+    styleUrls: ['./twitter.component.css']
 })
 export class TwitterComponent implements OnInit {
 
-  constructor(private _http : Http) {
-    var script = document.createElement('script');
-    script.src = '/assets/widgets.js';
-    script.type = 'text/javascript';
-    var head = document.getElementsByTagName("head")[0];
-    head.appendChild(script);
-  }
+    public isUpdate = true;
 
-  generateTweet() {
-    this._http.post("http://35.189.250.254:3000/tweet", {}).subscribe((res : Response) => {
-      console.log(res.toString());
-    });
-  }
-  ngOnInit() {
+    constructor(private _http: Http, private router: Router) {
+        this.createScript();
+    }
 
-  }
+    private createScript() {
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = "/assets/widgets.js"
+        script.id = "1";
+        var head = document.getElementsByTagName("head")[0];
+        head.appendChild(script);
+    }
+
+    generateTweet() {
+        this._http.post("http://35.189.250.254:3000/tweet", {}).subscribe((res: Response) => {
+            console.log(res.toString());
+        });
+
+        let script = document.getElementById("1");
+        script.parentNode.removeChild(script);
+        this.isUpdate = false;
+        setTimeout(() => {
+            this.createScript();
+           this.isUpdate = true;
+        }, 1000);
+    }
+
+    ngOnInit() {
+
+    }
 
 }
