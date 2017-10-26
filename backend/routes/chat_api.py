f8cd48dcd8a1c6ca0a9b6fdf423cce00b50a13b5
ranshamay89 <ran.shamay89@gmail.com>
2017-10-26 17:21:58 +0300

    chatapi fix

--- backend/routes/chat_api.py
+++ backend/routes/chat_api.py
@@ -1,4 +1,4 @@
-# from controllers.chat_controller import ChatController
+from controllers.chat_controller import ChatController
 from routes.Route import Route
 from server_utils import create_get_response, get_body_content, create_not_found_response
 
@@ -9,7 +9,7 @@ NAME = "chat_api"
 class ChatApi(Route):
     def __init__(self):
         Route.__init__(self, NAME, PATH)
-        # controller = ChatController()
+        controller = ChatController()
         bp = self._bp
 
         @bp.route('', methods=["POST"])
@@ -17,6 +17,7 @@ class ChatApi(Route):
             from flask import request
             body = get_body_content(request)
             if 'msg' in body:
-                return create_get_response({"response": body.get('msg')})
+                response = controller._anwer(body.get('msg'))
+                return create_get_response({"response": response})
             else:
                 return create_not_found_response()
