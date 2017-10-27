c7414380c951dadd74aa27d3003b85e7145c410c
ranshamay89 <ran.shamay89@gmail.com>
2017-10-27 07:41:40 +0300

    changed calling speech generator

--- backend/controllers/speech_controller.py
+++ backend/controllers/speech_controller.py
@@ -1,17 +1,22 @@
 from controllers.Controller import Controller
 from deep_bb import constants
+import re
 from deep_bb.generate_speech import MccSpeechGenerator
 import random
 import traceback
+
 DEEP_BB_PREFIX_PATH = 'deep_bb/deep_bb/'
 
 
 class SpeechController(Controller):
     def __init__(self):
         Controller.__init__(self)
-        with open(constants.CORPUS_PATH, 'r') as corpus:
-            text = corpus.read().lower()
-        self.mcc_speech_generator = MccSpeechGenerator().preprocess(text)
+
+        texts = ''
+        for path in constants.PROCESSED_SPEECHES_PATHS:
+            with open(path, 'r') as corpus:
+                texts += re.sub(pattern='\n+', repl='\n\n', string=corpus.read().lower()).strip() + '\n\n'
+        self.mcc_speech_generator = MccSpeechGenerator().preprocess(texts)
 
     def _generate_speech(self):
         data = None
