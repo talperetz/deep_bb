3b19fa74dffdf8e6ea88f5aee0fd61892be8288e
ranshamay89 <ran.shamay89@gmail.com>
2017-10-29 11:08:08 +0200

    reading chat from multiple resources

--- backend/controllers/chat_controller.py
+++ backend/controllers/chat_controller.py
@@ -19,11 +19,18 @@ possible_answers = ['im on a meeting with Trump, try again later',
 class ChatController(Controller):
     def __init__(self):
         Controller.__init__(self)
+
         self._qr = QueryResponder()
         with open(constants.PROCESSED_QNA_PATH) as f:
             qna_list = pickle.load(f)
         with open(constants.PROCESSED_QUOTES_PATH) as f:
             text = f.read().lower().decode('utf-8').strip()
+        with open(constants.PROCESSED_FB_POSTS_PATH) as f:
+            text = text + '\n\n' + f.read().lower().decode('utf-8').strip()
+        for path in constants.PROCESSED_SPEECHES_PATHS:
+            with open(path, 'r') as corpus:
+                text += re.sub(pattern='\n+', repl='\n\n', string=corpus.read().lower()).decode(
+                    'utf-8').strip() + '\n\n'
         tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
         sentences = tokenizer.tokenize(text)
         if not os.path.exists(constants.CHAT_INFERSENT_MODEL_PATH):
